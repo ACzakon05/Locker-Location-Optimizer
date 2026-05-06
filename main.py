9d3e891d866589cd18398ccffe5e726aa0882bd8
Adam Czakon <czakonadam@gmail.com>
2026-05-06 02:22:49 +0200

    Added Jastrzebie, changed main and scoring

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from src.pipeline import run_pipeline
-from src.cities import load_cities, get_bbox
+from src.pipeline import pipeline
+from src.cities import load_cities, get_bbox, get_expanded_bbox
 
 if __name__ == "__main__":
     cities = load_cities()
-    bbox = get_bbox("Krakow", cities)
 
-    _, _, recs = run_pipeline(bbox)
+    city_name = "Krakow"
+
+    bbox = get_bbox(city_name, cities)
+    expanded_bbox = get_expanded_bbox(city_name, cities)
+
+    _, _, recs = pipeline(expanded_bbox, bbox)
 
     print(recs)
--- src/scoring.py
+++ src/scoring.py
@@ -11,8 +11,8 @@ def compute_scores(df):
     df["raw_gap"]=1/ (1+ df["locker_density"])
     df[[f"{c}_norm" for c in cols_to_scale]] = scaler.fit_transform(df[cols_to_scale])
     df["score"] = (
-        0.7 * df["population_norm"] + 
-        0.2 * df["distance_norm"] + 
+        0.8 * df["population_norm"] + 
+        0.1 * df["distance_norm"] + 
         0.1 * df["raw_gap_norm"]
     )
     return df
